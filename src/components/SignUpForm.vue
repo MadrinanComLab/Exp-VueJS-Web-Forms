5765df07178fbed7e0a40c98bc5fff17c19ceedb
MadrinanComLab <johncliffordmadrinan838@gmail.com>
2022-06-23 20:30:57 +0700

    Simple validation was created

--- src/components/SignUpForm.vue
+++ src/components/SignUpForm.vue
@@ -1,10 +1,14 @@
 <template>
-  <form>
+  <!--/ BY DEFAULT, IF A SUBMIT BUTTON WAS CLICKED THE PAGE WILL BE REFRESH /-->
+  <!--/ BUT USING @submit.prevent WILL PREVENT THIS DEFAULT ACTION /-->
+  <!--/ .prevent IS JUST AN EVENT MODIFIER, YOU CAN USE @submit ONLY, BUT THAT WILL REFRESH THE PAGE ON SUBMISSION AS DEFAULT /-->
+  <form @submit.prevent="handleSubmit">
         <label>E-mail:</label>
         <input type="email" required v-model="email"><!--/ v-model ALLOWS US TO SAVE THE USER INPUT TO A VARIABLE. WHAT v-model DOES IS CALLED 2-WAY DATA BINDING /-->
       
         <label>Password:</label>
         <input type="password" required v-model="password">
+        <div v-show="passwordErr" class="error">{{ passwordErr }}</div>
       
         <label>Role:</label>
         <select v-model="role">
@@ -25,6 +29,10 @@
             <label>Accept terms and conditions</label>
         </div>
 
+        <div class="submit">
+            <button>Create an Account</button>
+        </div>
+
         <!--/ SECOND WAY OF USING v-model TO A CHECKBOX /-->
         <!--/ <div> v-model ALLOWS US TO SELECT MULTPLE VALUE BY STORING IT IN AN ARRAY
             <input type="checkbox" value="John" v-model="names">
@@ -38,24 +46,25 @@
         </div> /-->
     </form>
 
-    <p>Email: {{ email }}</p>
+    <!--/ <p>Email: {{ email }}</p>
     <p>Pasword: {{ password }}</p>
     <p>Role: {{ role }}</p>
     <p>Agrees to terms and conditions: {{ agreed }}</p>
-    <!--/ <p>Names: {{ names }}</p> /-->
+    <p>Names: {{ names }}</p> /-->
 </template>
 
 <script>
 export default {
     data() {
         return {
-            email: "name@example.com", // THIS WILL SET AS DEFAULT VALUE OF EMAIL INPUT FIELD
+            email: "", // THIS WILL SET AS DEFAULT VALUE OF EMAIL INPUT FIELD
             password: "",
             role: "developer", // THIS WILL SET AS DEFAULT VALUE OF COMBO BOX (<select>) TO Web Developer
             agreed: false,
             names: [],
             tempSkill: "",
-            skills: []
+            skills: [],
+            passwordErr: ""
         }
     },
 
@@ -79,6 +88,19 @@ export default {
             this.skills = this.skills.filter((item) => {
                 return skill !== item
             })
+        },
+
+        handleSubmit() {
+            this.passwordErr = this.password.length > 5 ? "" : "Password must be at least 6 characters long"
+
+            if (!this.passwordErr)
+            {
+                console.log("Email: " + this.email)
+                console.log("Password: " + this.password)
+                console.log("Role: " + this.role)
+                console.log("Skills: " + this.skills)
+                console.log("Terms Accepted: " + this.agreed)
+            }
         }
     }
 }
@@ -133,5 +155,25 @@ export default {
         font-weight: bold;
         color: #777;
         cursor: pointer;
+    }
+
+    button {
+        background: #0B6DFF;
+        border: 0;
+        padding: 10px 20px;
+        margin-top: 20px;
+        color: #FFF;
+        border-radius: 20px;
+    }
+
+    .submit {
+        text-align: center;
+    }
+
+    .error {
+        color: #FF0062;
+        margin-top: 10px;
+        font-size: 0.8em;
+        font-weight: bold;
     }
 </style>
